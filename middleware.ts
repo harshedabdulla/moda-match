76a81be197bc05cd7adb64992752c074f2a199d7
harshedabdulla <harshed2004@gmail.com>
2024-07-30 13:10:45 +0530

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+export default clerkMiddleware({
+  // "/" will be accessible to all users
+  authorizedParties: ["/api/webhooks/clerk"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
