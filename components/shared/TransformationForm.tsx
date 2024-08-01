32a10037486740d9371e7ed29070f558932b55dd
harshedabdulla <harshed2004@gmail.com>
2024-08-02 00:23:05 +0530

    mongodb connection

---
+++ components/shared/TransformationForm.tsx
@@ -0,0 +1,54 @@
+"use client"
+import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { title } from 'process'
+import { defaultValues } from '@/constants'
+
+export const formSchema = z.object({
+    title: z.string(),
+    aspectRatio: z.string().optional(),
+    color: z.string().optional(),
+    prompt: z.string().optional(),
+    publicId: z.string(),
+
+})
+
+const TransformationForm = ({action,data=null}: TransformationFormProps) => {
+    const initialValues = data && action === "Update" ? {
+        title: data?.title,
+        aspectRatio: data?.aspectRatio,
+        color: data?.color,
+        prompt: data?.prompt,
+        publicId: data?.publicId,
+    } : defaultValues
+
+    const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues,
+    })
+ 
+    function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+    }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        
+      </form>
+    </Form>
+  )
+}
+
+export default TransformationForm
